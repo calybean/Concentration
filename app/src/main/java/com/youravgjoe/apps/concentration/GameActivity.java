a44cdcd8b48f15aa66e4b8d7f33b58047a7b80f6
youravgjoe <joeycannon1@gmail.com>
2016-11-04 17:14:00 -0600

    Added ability to import photos and use them in the game

--- app/src/main/java/com/youravgjoe/apps/concentration/GameActivity.java
+++ app/src/main/java/com/youravgjoe/apps/concentration/GameActivity.java
@@ -1,15 +1,28 @@
 package com.youravgjoe.apps.concentration;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -17,8 +30,17 @@ import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
+    private static final String TAG = "GameActivity";
+
+    private static final String PHOTO_LIST = "PHOTO_LIST";
+
+    private boolean usePhotos = false;
+
+    List<String> mFilePathList;
+
     LinearLayout mGameLayout;
     List<Integer> mCardList; // list of image resource ids to each card image
+    List<Drawable> mDrawableList; // list of drawables if they've imported photos
 
     List<Integer> mMatches = new ArrayList<>();
 
@@ -31,6 +53,12 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         mGameLayout = (LinearLayout) findViewById(R.id.game_layout);
+
+        if (getIntent().getExtras() != null) {
+            mFilePathList = getIntent().getStringArrayListExtra(PHOTO_LIST);
+            mDrawableList = new ArrayList<>();
+            usePhotos = true;
+        }
 
         setupGame();
     }
@@ -65,10 +93,20 @@ public class GameActivity extends AppCompatActivity {
                         }
                         if (mImageOne == null) {
                             mImageOne = (ImageView) findViewById(image.getId());
-                            mImageOne.setImageDrawable(getResources().getDrawable(mCardList.get(image.getId())));
+                            mImageOne.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                            if (usePhotos) {
+                                mImageOne.setImageDrawable(mDrawableList.get(image.getId()));
+                            } else {
+                                mImageOne.setImageDrawable(getResources().getDrawable(mCardList.get(image.getId())));
+                            }
                         } else if (mImageTwo == null) {
                             mImageTwo =  (ImageView) findViewById(image.getId());
-                            mImageTwo.setImageDrawable(getResources().getDrawable(mCardList.get(image.getId())));
+                            mImageTwo.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                            if (usePhotos) {
+                                mImageTwo.setImageDrawable(mDrawableList.get(image.getId()));
+                            } else {
+                                mImageTwo.setImageDrawable(getResources().getDrawable(mCardList.get(image.getId())));
+                            }
 
                             // we found a match!
                             if (mImageOne.getDrawable().getConstantState().equals(mImageTwo.getDrawable().getConstantState())) {
@@ -87,9 +125,9 @@ public class GameActivity extends AppCompatActivity {
                             } else {
                                 // do nothing, just wait for them to click again?
                             }
-
                         } else {
-
+                            mImageOne.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                            mImageTwo.setScaleType(ImageView.ScaleType.FIT_CENTER);
                             mImageOne.setImageDrawable(getResources().getDrawable(R.drawable.ic_android_black_24dp));
                             mImageTwo.setImageDrawable(getResources().getDrawable(R.drawable.ic_android_black_24dp));
 
@@ -101,9 +139,11 @@ public class GameActivity extends AppCompatActivity {
 
                 CardView card = new CardView(this);
 
+                // todo: set card dimens as fixed, so they don't resize. Don't make them wrap_content, make them accurate dimens using screenWidth and screenHeight
+
                 // set card margins
                 int margin = 16;
-                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(screenWidth / 4, screenHeight / 4);
                 params.setMargins(margin, margin, margin, margin);
                 card.setLayoutParams(params);
 
@@ -129,25 +169,86 @@ public class GameActivity extends AppCompatActivity {
     private void randomizeCards() {
         // make list of available image resources
         // give two ids to each resource (0-15)
-        // edit the onclick listener to query the list for the image to display
 
         mCardList = new ArrayList<>();
 
-        // add each card twice
-        for (int i = 0; i < 2; i++) {
-            mCardList.add(R.drawable.ic_brightness_5_black_24dp);
-            mCardList.add(R.drawable.ic_insert_emoticon_black_24dp);
-            mCardList.add(R.drawable.ic_local_airport_black_24dp);
-            mCardList.add(R.drawable.ic_local_florist_black_24dp);
-            mCardList.add(R.drawable.ic_local_shipping_black_24dp);
-            mCardList.add(R.drawable.ic_phone_black_24dp);
-            mCardList.add(R.drawable.ic_star_black_24dp);
-            mCardList.add(R.drawable.ic_wifi_black_24dp);
+        if (mFilePathList.isEmpty()) {
+            // add each drawable id twice
+            for (int i = 0; i < 2; i++) {
+                mCardList.add(R.drawable.ic_brightness_5_black_24dp);
+                mCardList.add(R.drawable.ic_insert_emoticon_black_24dp);
+                mCardList.add(R.drawable.ic_local_airport_black_24dp);
+                mCardList.add(R.drawable.ic_local_florist_black_24dp);
+                mCardList.add(R.drawable.ic_local_shipping_black_24dp);
+                mCardList.add(R.drawable.ic_phone_black_24dp);
+                mCardList.add(R.drawable.ic_star_black_24dp);
+                mCardList.add(R.drawable.ic_wifi_black_24dp);
+            }
+            // shuffle the cards
+            long seed = System.nanoTime();
+            Collections.shuffle(mCardList, new Random(seed));
+        } else {
+            for (String filepath : mFilePathList) {
+                // create a new drawable for each photo, and add each twice
+                Drawable d = new BitmapDrawable(getResources(), getBitmapFromFilePath(filepath));
+                mDrawableList.add(d);
+                mDrawableList.add(d);
+            }
+            // shuffle the cards
+            long seed = System.nanoTime();
+            Collections.shuffle(mDrawableList, new Random(seed));
+        }
+    }
+
+    private Bitmap getBitmapFromFilePath(String filepath) {
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filepath, options);
+
+        final double heightWidthRatio = (double)options.outHeight / (double)options.outWidth;
+
+        WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        final int dpHeight = 100;
+
+        // convert dp to pixels
+        float scale = getResources().getDisplayMetrics().density;
+        int reqHeight = (int) (dpHeight * scale + 0.5f);
+        double reqWidth = (int) ((dpHeight / heightWidthRatio) * scale + 0.5f);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, (int)reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+
+        return BitmapFactory.decodeFile(filepath, options);
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
         }
 
-        // shuffle the cards
-        long seed = System.nanoTime();
-        Collections.shuffle(mCardList, new Random(seed));
+        return inSampleSize;
     }
 
     private int convertPixelsToDp(int pixels) {
--- app/src/main/java/com/youravgjoe/apps/concentration/MainActivity.java
+++ app/src/main/java/com/youravgjoe/apps/concentration/MainActivity.java
@@ -6,14 +6,18 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Display;
@@ -22,20 +26,32 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.content.Intent.EXTRA_ALLOW_MULTIPLE;
+import static android.view.View.GONE;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private static final int SELECT_PICTURE = 47;
-    private static final int PERMISSION_REQUEST_READ_FILES = 47;
+    private static final int PERMISSION_REQUEST_WRITE_FILES = 47;
+    private static final String PHOTO_LIST = "PHOTO_LIST";
+    private ArrayList<String> mFilePathList;
     private LinearLayout mImageContainer;
+    private TextView mPhotoCountTextView;
+    private TextView mPhotoPromptTextView;
+    private int mPhotoCount;
+    private Button mAddPhotosButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,22 +61,31 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mImageContainer = (LinearLayout) findViewById(R.id.image_container);
+        mPhotoCountTextView = (TextView) findViewById(R.id.photo_count_textview);
+        mPhotoPromptTextView = (TextView) findViewById(R.id.photo_prompt_textview);
+        mAddPhotosButton = (Button) findViewById(R.id.add_photos);
+        mFilePathList = new ArrayList<>();
 
         Button startGame = (Button) findViewById(R.id.start_game);
         startGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, GameActivity.class));
+                if (mPhotoCount == 8) {
+                    Intent newGameIntent = new Intent(MainActivity.this, GameActivity.class);
+                    newGameIntent.putStringArrayListExtra(PHOTO_LIST, mFilePathList);
+                    startActivity(newGameIntent);
+                } else {
+                    startActivity(new Intent(MainActivity.this, GameActivity.class));
+                }
             }
         });
 
-        Button addPhotosButton = (Button) findViewById(R.id.add_photos);
-        addPhotosButton.setOnClickListener(new View.OnClickListener() {
+        mAddPhotosButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 if (ContextCompat.checkSelfPermission(v.getContext(),
-                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
                         != PackageManager.PERMISSION_GRANTED) {
                     // if we don't have the permission yet, get it.
                     checkPermission();
@@ -102,7 +127,6 @@ public class MainActivity extends AppCompatActivity {
                     final double heightWidthRatio = (double)options.outHeight / (double)options.outWidth;
                     Log.d(TAG, "heightWidthRatio: " + heightWidthRatio);
 
-
                     WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
                     Display display = wm.getDefaultDisplay();
                     Point size = new Point();
@@ -110,10 +134,7 @@ public class MainActivity extends AppCompatActivity {
 
                     final int dpHeight = 100;
 
-                    // todo: something about height and width is still not working.
-                    // todo: we should also probably upload these photos sometime...
-
-                    // convert pixels to dp
+                    // convert dp to pixels
                     float scale = getResources().getDisplayMetrics().density;
                     int reqHeight = (int) (dpHeight * scale + 0.5f);
                     double reqWidth = (int) ((dpHeight / heightWidthRatio) * scale + 0.5f);
@@ -127,8 +148,32 @@ public class MainActivity extends AppCompatActivity {
                     options.inJustDecodeBounds = false;
                     Bitmap bitmap = BitmapFactory.decodeStream(newBufferedInputStream, null, options);
 
+                    // add photo to a card, and then to the horizontal scroll view
                     addPhotoToContainer(bitmap);
 
+                    // CALL THIS METHOD TO GET THE URI FROM THE BITMAP
+                    Uri tempUri = getImageUri(getApplicationContext(), bitmap);
+
+                    // CALL THIS METHOD TO GET THE ACTUAL PATH
+                    File finalFile = new File(getRealPathFromURI(tempUri));
+
+                    // add filepath to list
+                    mFilePathList.add(finalFile.getAbsolutePath());
+
+                    // increment photo count and update view
+                    mPhotoCount++;
+                    mPhotoCountTextView.setText(getResources().getString(R.string.photo_count, mPhotoCount));
+
+                    // if this is the first time we're adding a photo, make the prompt visible
+                    if (mPhotoPromptTextView.getVisibility() == GONE) {
+                        mPhotoPromptTextView.setVisibility(View.VISIBLE);
+                    }
+
+                    // if we've reached 8 photos, disable the add button and color it grey
+                    if (mPhotoCount == 8) {
+                        mAddPhotosButton.setEnabled(false);
+                        mAddPhotosButton.setBackgroundColor(getResources().getColor(R.color.grey));
+                    }
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 }
@@ -137,6 +182,22 @@ public class MainActivity extends AppCompatActivity {
             //Display an error
             Toast.makeText(this, "Error retrieving photo", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap bitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), bitmap, "Title", null);
+        return Uri.parse(path);
+    }
+
+    public String getRealPathFromURI(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+        String filePath = cursor.getString(idx);
+        cursor.close();
+        return filePath;
     }
 
     public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
@@ -163,42 +224,50 @@ public class MainActivity extends AppCompatActivity {
 
     private void addPhotoToContainer(Bitmap bmp) {
         ImageView imageView = new ImageView(this);
-
-        // convert pixels to dp for margin
-        float scale = getResources().getDisplayMetrics().density;
-        int dp = (int) (8 * scale + 0.5f);
-
-        if (mImageContainer.getChildCount() == 0) {
-            imageView.setPadding(0, 0, 0, 0); // left, top, right, bottom
-        } else {
-            imageView.setPadding(dp, 0, 0, 0); // left, top, right, bottom
-        }
         imageView.setImageBitmap(bmp);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
 
-        mImageContainer.addView(imageView);
+        CardView card = new CardView(this);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(dpToPixels(200), dpToPixels(200));
+        params.setMargins(dpToPixels(4),dpToPixels(4),dpToPixels(4),dpToPixels(4));
+
+        card.setLayoutParams(params);
+        card.setContentPadding(dpToPixels(8), dpToPixels(8), dpToPixels(8), dpToPixels(8));
+        card.setMaxCardElevation(5);
+        card.setCardElevation(9);
+
+        card.addView(imageView);
+
+        mImageContainer.addView(card);
+    }
+
+    private int dpToPixels(int dp) {
+        float scale = getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
     }
 
     public void checkPermission() {
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_EXTERNAL_STORAGE)
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
 
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
 
                 showMessageOKCancel("Concentration would like permission to access your photos.",
                         new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 ActivityCompat.requestPermissions(MainActivity.this,
-                                        new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
-                                        PERMISSION_REQUEST_READ_FILES);
+                                        new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                        PERMISSION_REQUEST_WRITE_FILES);
                             }
                         });
             } else {
                 ActivityCompat.requestPermissions(this,
-                        new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
-                        PERMISSION_REQUEST_READ_FILES);
+                        new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        PERMISSION_REQUEST_WRITE_FILES);
             }
         }
     }
